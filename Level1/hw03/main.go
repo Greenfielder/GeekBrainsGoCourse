8368e7573bcba6a68451351c7c8a88977a8bf5a7
Dmitry <dimon-sad@mail.ru>
2021-10-01 22:44:14 +0200

    add some func to calculator

--- Level1/hw03/main.go
+++ Level1/hw03/main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 )
 
 func main() {
 
 	var operation string
+	fmt.Println("Введите нужную операцию (+, -, *, /, ^)")
 	fmt.Scan(&operation)
+
 	fmt.Println("Введите два операнда через пробел: ")
 	var operand1, operand2 float64
-	fmt.Scan(&operand1, &operand2) // validate
+	fmt.Scan(&operand1, &operand2)
 
 	var result float64
 	switch operation {
+
 	case "+":
 		result = operand1 + operand2
 	case "-":
@@ -22,7 +26,14 @@ func main() {
 	case "*":
 		result = operand1 * operand2
 	case "/":
-		result = operand1 / operand2 //validate /0
+		if operand2 == 0 {
+			fmt.Println("Делить на 0 нельзя!")
+			os.Exit(1)
+		} else {
+			result = operand1 / operand2
+		}
+	case "^":
+		result = math.Pow(operand1, operand2)
 	default:
 		fmt.Println("Введена неизвестная операция.")
 		os.Exit(1)
