d816b672d09501cf92ac84ac94197288d2fc7eef
Dmitry <dimon-sad@mail.ru>
2021-10-07 22:26:32 +0200

    Add fibbonachi func

---
+++ Level1/hw05/main.go
@@ -0,0 +1,18 @@
+package main
+
+// Home work 5
+
+func fibbonachi(n uint) uint {
+
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fibbonachi(n-1) + fibbonachi(n-2)
+}
+
+func main() {
+	println("Hello, world !")
+}
