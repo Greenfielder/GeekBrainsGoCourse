0aee0ac7864c269eac0ba8f4084d0d3e47fa7d28
Dmitry <dimon-sad@mail.ru>
2021-11-22 11:43:56 +0200

    repeat learning stuff

--- Level2/hw06/main.go
+++ Level2/hw06/main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 
-	fmt.Println("Start working")
+	// fmt.Println(runtime.NumCPU())
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for c := 'a'; c <= 'z'; c += 1 {
+			fmt.Printf("%c", c)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		for i := 0; i <= 10; i += 1 {
+			fmt.Printf("%d", i)
+		}
+	}()
+
+	wg.Wait()
+
 }
