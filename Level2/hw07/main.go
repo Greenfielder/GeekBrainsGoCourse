759db1e5894e5e1dfd8b89ccf4d51a71423199ca
Dmitry <dimon-sad@mail.ru>
2021-11-28 21:53:48 +0200

    some work

--- Level2/hw07/main.go
+++ Level2/hw07/main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
+type In struct {
+	Name   string
+	Cost   int
+	IsReal bool
+}
+
+func customFunc(in In, values map[string]interface{}) error {
+	valuesOfStrust := reflect.ValueOf(&in)
+	elementOfIn := valuesOfStrust.Elem()
+
+	for i := 0; i < elementOfIn.Type().NumField(); i++ {
+		tempName := elementOfIn.Type().Field(i).Name
+		newName := elementOfIn.FieldByName(tempName)
+		fmt.Println(tempName, newName, "-----")
+	}
+
+	return nil
+}
+
 func main() {
-	fmt.Println("Пока это заглушка, но скоро тут будет готовое ДЗ7")
+	newValues := map[string]interface{}{
+		"Name":   "Rick",
+		"Cost":   100,
+		"IsReal": true,
+	}
+
+	var inner In
+	//На данный момент, я не понимаю в какую сторону дальше двигаться, что бы получить нужный результат
+	customFunc(inner, newValues)
+	fmt.Println(inner)
 }
