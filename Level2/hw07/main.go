990c6b96ba258ef638e1f8333cabe9364e015fd7
Dmitry <dimon-sad@mail.ru>
2021-11-29 23:14:43 +0200

    new some work, but still didn't work

--- Level2/hw07/main.go
+++ Level2/hw07/main.go
@@ -14,25 +14,50 @@ type In struct {
 func customFunc(in In, values map[string]interface{}) error {
 	valuesOfStrust := reflect.ValueOf(&in)
 	elementOfIn := valuesOfStrust.Elem()
+	rv := reflect.ValueOf(values)
 
 	for i := 0; i < elementOfIn.Type().NumField(); i++ {
 		tempName := elementOfIn.Type().Field(i).Name
 		newName := elementOfIn.FieldByName(tempName)
-		fmt.Println(tempName, newName, "-----")
-	}
 
+		for _, e := range rv.MapKeys() {
+			v := rv.MapIndex(e)
+			switch t := v.Interface().(type) {
+
+			case int:
+				if i == 1 {
+					newName.SetInt(int64(t))
+				}
+			case string:
+				if i == 0 {
+					newName.SetString(t)
+				}
+			case bool:
+				if i == 2 {
+					newName.SetBool(t)
+				}
+			default:
+				fmt.Println("not found")
+			}
+		}
+	}
 	return nil
 }
 
+//Написать функцию, которая принимает на вход структуру in (struct или кастомную struct) и values map[string]interface{}
+//(key - название поля структуры, которому нужно присвоить value этой мапы).
+//Необходимо по значениям из мапы изменить входящую структуру in с помощью пакета reflect.
+//Функция может возвращать только ошибку error. Написать к данной функции тесты (чем больше, тем лучше - зачтется в плюс).
+
 func main() {
+
 	newValues := map[string]interface{}{
 		"Name":   "Rick",
 		"Cost":   100,
 		"IsReal": true,
 	}
-
-	var inner In
-	//На данный момент, я не понимаю в какую сторону дальше двигаться, что бы получить нужный результат
+	var inner = In{"John", 50, false}
+	//Не получается присвоить новые значения, хотя на момент присваивания корректно отбражаются и старые и новые значения.
 	customFunc(inner, newValues)
 	fmt.Println(inner)
 }
